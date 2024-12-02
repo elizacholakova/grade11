0067cf4b4863c93fce8904dc6a221e79d4453f2b
elizacholakova <e.cholakova26@acsbg.org>
2024-12-02 10:08:54 +0200

    CompositionTask.java
    Car, Wheel, Engine, etc,

---
+++ compositionTask.java
@@ -0,0 +1,61 @@
+public class Composition {
+    public static void main(String[] args) {
+        Engine eng1 = new Engine("V8");
+        Car car1 = new Car("Ford", eng1, 18);
+        car1.displayInfo();
+    }
+}
+
+class Engine {
+    private String type;
+
+    public Engine(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
+
+class Wheel {
+    private int size;
+
+    public Wheel(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
+
+class Car {
+    private String brand;
+    private Engine engine;
+    private Wheel frontLeftWheel;
+    private Wheel frontRightWheel;
+    private Wheel rearLeftWheel;
+    private Wheel rearRightWheel;
+
+    public Car(String brand, Engine engine, int wheelSize) {
+        this.brand = brand;
+        this.engine = engine;
+        this.frontLeftWheel = new Wheel(wheelSize);
+        this.frontRightWheel = new Wheel(wheelSize);
+        this.rearLeftWheel = new Wheel(wheelSize);
+        this.rearRightWheel = new Wheel(wheelSize);
+    }
+
+    public void displayInfo() {
+        System.out.println("Car Brand: " + brand);
+        System.out.println("Engine Type: " + engine.getType());
+        System.out.println("Front Left Wheel Size: " + frontLeftWheel.getSize() + " cm");
+        System.out.println("Front Right Wheel Size: " + frontRightWheel.getSize() + " cm");
+        System.out.println("Rear Left Wheel Size: " + rearLeftWheel.getSize() + " cm");
+        System.out.println("Rear Right Wheel Size: " + rearRightWheel.getSize() + " cm");
+    }
+    public String getBrand(){
+        return brand;
+    }
+}
